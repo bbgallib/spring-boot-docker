22df17bc450b79a3a57bfcf5bbddee641014db64
bbgallib <bbgallib@gmail.com>
2020-08-10 10:15:16 +0900

    Spring bootひな形作成

---
+++ src/main/java/com/example/sbdocker/SbdockerApplication.java
@@ -0,0 +1,21 @@
+package com.example.sbdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SbdockerApplication {
+
+	@RequestMapping("/")
+	String Index() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbdocker/SbdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
