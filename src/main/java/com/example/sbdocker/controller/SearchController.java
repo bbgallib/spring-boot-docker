22293f96c574ad4f11d7bf1f606a9e708dccdc87
bbgallib <bbgallib@gmail.com>
2020-08-10 18:16:33 +0900

    DB初期データ追加

--- src/main/java/com/example/sbdocker/controller/SearchController.java
+++ src/main/java/com/example/sbdocker/controller/SearchController.java
@@ -3,7 +3,6 @@ package com.example.sbdocker.controller;
 import com.example.sbdocker.domain.model.resource.SearchResponseResource;
 import com.example.sbdocker.domain.service.SearchService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 public class SearchController {
 
@@ -23,7 +21,6 @@ public class SearchController {
             @RequestParam(value = "unitName", required = false) String unitName
     ) {
         List<SearchResponseResource> resources = searchService.selectProfiles(name, unitName);
-        System.out.println(resources);
         return resources;
     }
 }
