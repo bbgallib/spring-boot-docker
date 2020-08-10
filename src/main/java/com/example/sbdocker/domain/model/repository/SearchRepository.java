8ff31984ed248e1335f828e5669c679b99d5a1a7
bbgallib <bbgallib@gmail.com>
2020-08-10 12:53:41 +0900

    Docker上でMySQLコンテナを作成

---
+++ src/main/java/com/example/sbdocker/domain/model/repository/SearchRepository.java
@@ -0,0 +1,9 @@
+package com.example.sbdocker.domain.model.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class SearchRepository {
+}
---
+++ src/main/java/com/example/sbdocker/infrastracture/mapper/SearchMapperXml.java
@@ -0,0 +1,7 @@
+package com.example.sbdocker.infrastracture.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SearchMapperXml {
+}
